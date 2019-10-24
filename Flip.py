dc0700a24673dcbb209ea62c2bf5518a39af8d16
Felix Suen <felix.suen2000@gmail.com>
2019-10-24 14:47:57 -0400

    Runtimer

--- Flip.py
+++ Flip.py
@@ -1,10 +1,12 @@
 import random
 import matplotlib.pyplot as plt
+import time
 
 
 def flip():
     print('number of flips: ')
     num = input()
+    startTime = time.time()
 
     try:
         num = int(num)
@@ -22,17 +24,22 @@ def flip():
     print('head: ' + str(head))
     print('tail: ' + str(num - head))
 
+    endTime = time.time()
+    total = round(endTime - startTime, 4)
+    print('%s secs to flip %s coins' % (total, num))
+
     # graphing
     names = 'Head', 'Tail'
     scores = [head, num-head]
     percent = [head/num*100, (num-head)/num*100]
 
     fig, axs = plt.subplots(2)
-    fig.suptitle('Coin Flip')
+    fig.suptitle('%s Flips \n %s Heads vs %s Tails' % (num, head, num-head))
     axs[0].bar(names, scores, align='center', alpha=0.5, color='red')
 
     axs[1].pie(percent, labels=names, startangle=90, autopct='%1.0f%%')
     axs[1].axis('equal')
+    axs[1].set_xlabel('%s seconds' % total)
     plt.subplots_adjust(bottom=0.04, hspace=0.3)
     plt.show()
 
